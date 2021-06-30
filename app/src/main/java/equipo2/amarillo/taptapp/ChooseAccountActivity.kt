d166a697c208090c3aab9c33550fef38c18018b6
XxPanchoPanteraxX <78341976+XxPanchoPanteraxX@users.noreply.github.com>
2021-06-30 12:01:07 -0700

    pantallas-login-añadido

---
+++ app/src/main/java/equipo2/amarillo/taptapp/ChooseAccountActivity.kt
@@ -0,0 +1,11 @@
+package equipo2.amarillo.taptapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_account)
+    }
+}
