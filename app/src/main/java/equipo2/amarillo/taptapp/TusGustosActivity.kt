1bc205cad3f49d31fe2364ca9883c8b22bd31079
RojoAder <alexrojo6@hotmail.com>
2021-07-01 17:55:37 -0700

    Tus gustos terminados

---
+++ app/src/main/java/equipo2/amarillo/taptapp/TusGustosActivity.kt
@@ -0,0 +1,11 @@
+package equipo2.amarillo.taptapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TusGustosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tus_gustos)
+    }
+}
