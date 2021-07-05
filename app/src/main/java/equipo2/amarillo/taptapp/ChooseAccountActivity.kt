795ca4a469c649651e20faaed88b6b7735edb360
AderRojo <alexrojo6@hotmail.com>
2021-07-04 22:17:55 -0700

    navegación hecha

--- app/src/main/java/equipo2/amarillo/taptapp/ChooseAccountActivity.kt
+++ app/src/main/java/equipo2/amarillo/taptapp/ChooseAccountActivity.kt
@@ -1,11 +1,26 @@
 package equipo2.amarillo.taptapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ChooseAccountActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_account)
+
+        val login_google = findViewById(R.id.login_google) as Button
+        val login_fb = findViewById(R.id.login_fb) as Button
+
+        login_google.setOnClickListener {
+            var intent: Intent = Intent(this, TusGustosActivity::class.java)
+            this.startActivity(intent)
+        }
+
+        login_fb.setOnClickListener {
+            var intent: Intent = Intent(this, TusGustosActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/equipo2/amarillo/taptapp/TusGustosActivity.kt
+++ app/src/main/java/equipo2/amarillo/taptapp/TusGustosActivity.kt
@@ -1,11 +1,20 @@
 package equipo2.amarillo.taptapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TusGustosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tus_gustos)
+
+        val btn_listo = findViewById(R.id.btn_listo) as Button
+
+        btn_listo.setOnClickListener {
+            var intent: Intent = Intent(this, MainActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
