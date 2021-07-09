94a7604b5ce826ed4f71c6ac493715a5b1cfb3d0
RojoAder <alexrojo6@hotmail.com>
2021-07-08 18:15:31 -0700

    login google

--- app/src/main/java/equipo2/amarillo/taptapp/ChooseAccountActivity.kt
+++ app/src/main/java/equipo2/amarillo/taptapp/ChooseAccountActivity.kt
@@ -4,23 +4,100 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 
 class ChooseAccountActivity : AppCompatActivity() {
+
+    val RC_SIGN_IN = 123
+    val COD_LOGOUT = 323
+    lateinit var mGoogleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         val login_google = findViewById(R.id.login_google) as Button
         val login_fb = findViewById(R.id.login_fb) as Button
 
         login_google.setOnClickListener {
             var intent: Intent = Intent(this, TusGustosActivity::class.java)
-            this.startActivity(intent)
+            this.startActivityForResult(intent, RC_SIGN_IN)
         }
 
         login_fb.setOnClickListener {
             var intent: Intent = Intent(this, TusGustosActivity::class.java)
             this.startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
+        if (requestCode == COD_LOGOUT) {
+            signOut()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        // Check for existing Google Sign In account, if the user is already signed in
+// the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        updateUI(account)
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+            .addOnCompleteListener(this) {
+                Toast.makeText(this, "Sesión Terminada", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.w(TAG, "signInResult:failed code=" + e.statusCode)
+            updateUI(null)
+        }
+
+
+    }
+
+    private fun updateUI(account: GoogleSignInAccount?) {
+        if (account != null) {
+            val intent = Intent(this, TusGustosActivity::class.java)
+            intent.putExtra("name", account.displayName)
+            intent.putExtra("email", account.email)
+            startActivityForResult(intent, COD_LOGOUT)
         }
     }
 }
