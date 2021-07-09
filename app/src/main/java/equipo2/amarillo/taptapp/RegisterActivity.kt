22cbb30b732c68b6ffa6f7bc017d424512e573c8
RojoAder <alexrojo6@hotmail.com>
2021-07-09 14:31:24 -0700

    welcome layout finished

---
+++ app/src/main/java/equipo2/amarillo/taptapp/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package equipo2.amarillo.taptapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
