cb1c9ed843cd7db2c07b74d77d1300746d526fe0
AderRojo <alexrojo6@hotmail.com>
2021-07-15 22:47:03 -0700

    idk

--- app/src/main/java/equipo2/amarillo/taptapp/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/equipo2/amarillo/taptapp/ui/dashboard/DashboardFragment.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import equipo2.amarillo.taptapp.R
 import equipo2.amarillo.taptapp.databinding.FragmentDashboardBinding
+import equipo2.amarillo.taptapp.ui.recetas.fragment_recetas
 
 class DashboardFragment : Fragment() {
 
@@ -31,10 +35,19 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       // val textView: TextView = binding.textDashboard
-        //dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-         //   textView.text = it
-        //})
+        val receta1_img = root.findViewById(R.id.receta1_img) as ImageView
+        val receta2_img = root.findViewById(R.id.receta2_img) as ImageView
+        val receta3_img = root.findViewById(R.id.receta3_img) as ImageView
+        val btn_vegetariano = root.findViewById(R.id.vegetariano_btn) as ImageButton
+        val btn_vegano = root.findViewById(R.id.vegano_btn) as ImageButton
+
+        receta1_img.setOnClickListener {
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.replace(R.id.navigation_dashboard,fragment_recetas())
+            transaction?.disallowAddToBackStack()
+            transaction?.commit()
+        }
+
         return root
     }
 
